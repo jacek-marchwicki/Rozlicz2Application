57c7b15566ee63f2034d624fe1958a9b5cfbc7c4
Jacek Marchwicki <jacek@3made.eu>
2011-08-22 11:32:32 +0200

    Added example test

---
+++ test/com/rozlicz2/application/TestTesting.java
@@ -0,0 +1,12 @@
+package com.rozlicz2.application;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTesting {
+
+	@Test
+	public void testTesting() {
+		assertEquals("fail", 50, 50);
+	}
+}
